24ffcf3e87649869ea62169cef328cd09585b40c
Cynthia Mbulu <72040367+CyntiBinti@users.noreply.github.com>
2021-06-26 21:35:48 +0100

    Solution!

--- file.js
+++ file.js
@@ -2,9 +2,8 @@ function filteredArray(arr, elem) {
   let newArr = [];
   // Only change code below this line
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == arr.indexOf(elem)) {
-            arr.splice((arr[i]));
-            newArr.push(arr);
+        if (arr[i].indexOf(elem) == -1) {
+            newArr.push(arr[i]);
         }
     }
   // Only change code above this line
